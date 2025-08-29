1e3df289320b5c1174a3d0c1373d820e0031ff11
HasutoSasaki <hasuto.sa@gmail.com>
2025-08-30 08:33:05 +0900

    feat: add ModelDuck and FlyRocketPowered classes, and update MiniDuckSimulator to demonstrate new behaviors

--- examples/java/basic/Duck.java
+++ examples/java/basic/Duck.java
@@ -18,4 +18,12 @@ public abstract class Duck {
     public void swim() {
         System.out.println("すべてのカモは浮きます。おとりのカモでも！");
     }
+
+    public void setFlyBehavior(FlyBehavior fb) {
+        flyBehavior = fb;
+    }
+
+    public void setQuackBehavior(QuackBehavior gb) {
+        quackBehavior = gb;
+    }
 }
